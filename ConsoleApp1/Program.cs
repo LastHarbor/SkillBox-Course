0fc60409800dee1f4c6459e46154e6ad16d0a3e9
Anton <Anton@DESKTOP-850KSKQ>
2022-06-12 13:42:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!!!\n");      //Вывод на консоль "Hello, World!"
+            Console.Write("Hello,"); Console.Write(" World"); Console.Write("!!!"); //Метод вывода без переноса строки
+            Console.ReadLine();
+        }
+    }
+}
